beb3dd73a734f64f03baf412c5f278eb494674ca
Michael Bartl <bar16091@spengergasse.at>
2017-08-02 15:39:59 +0200

    added register route, fixes #9

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from 'pages/Home/Home'
 import NewProject from 'pages/Project/New/New'
 import Profile from 'pages/Profile/Profile'
 import Login from 'pages/Login/Login'
+import Register from 'pages/Register/Register'
 
 class App extends Component {
   render() {
@@ -19,13 +20,15 @@ class App extends Component {
 				  <li><Link to='/'>Home</Link></li>
 				  <li><Link to='/new'>Create New Project</Link></li>
 				  <li><Link to='/profile'>Profile Page</Link></li>
-				  <li><Link to='/login'>Profile Page</Link></li>
+				  <li><Link to='/login'>Login</Link></li>
+				  <li><Link to='/register'>Register</Link></li>
 			  </ul>
-			  
+
 			  <Route exact path='/' component={Home} />
 			  <Route path='/new' component={NewProject} />
 			  <Route path='/profile' component={Profile} />
 			  <Route path='/login' component={Login} />
+			  <Route path='/register' component={Register} />
 		  </div>
 	  </Router>
     )
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+	render() {
+		return (
+			<div>
+				<h1>projectAlpha</h1>
+				<p>Register</p>
+			</div>
+		)
+	}
+}
+
+export default Register
