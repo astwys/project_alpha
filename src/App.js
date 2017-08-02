adfb6e9b25dbd64ba1c771cef6082e31f26521d1
Michael Bartl <bar16091@spengergasse.at>
2017-08-02 15:29:50 +0200

    added create project route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom'
 
 import Home from 'sites/Home/Home'
+import NewProject from 'sites/Project/New/New'
 
 class App extends Component {
   render() {
@@ -14,8 +15,10 @@ class App extends Component {
 		  <div>
 			  <ul>
 				  <li><Link to='/'>Home</Link></li>
+				  <li><Link to='/new'>Create New Project</Link></li>
 			  </ul>
 			  <Route exact path='/' component={Home} />
+			  <Route path='/new' component={NewProject} />
 		  </div>
 	  </Router>
     )
---
+++ src/sites/Project/New/New.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<h1>projectAlpha</h1>
+				<p>New Project</p>
+			</div>
+		)
+	}
+}
+
+export default Home
