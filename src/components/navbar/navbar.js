5097acb0953e4b12672d0da59b96290327224e44
Michael Bartl <bar16091@spengergasse.at>
2017-08-02 20:00:15 +0200

    styled navbar, fixes #23

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './navbar.css'
 
 class Navbar extends Component {
 	render() {
 		return (
-			<ul>
-				<li><Link to='/'>Home</Link></li>
-				<li><Link to='/new'>Create New Project</Link></li>
-				<li><Link to='/profile'>Profile Page</Link></li>
-				<li><Link to='/login'>Login</Link></li>
-				<li><Link to='/register'>Register</Link></li>
-			</ul>
+			<div className="navbar">
+				<ul>
+					<li><Link className="link" to='/'>Home</Link></li>
+					<li><Link className="link" to='/new'>Create New Project</Link></li>
+					<li><Link className="link" to='/profile'>Profile Page</Link></li>
+					<li><Link className="link" to='/login'>Login</Link></li>
+					<li><Link className="link" to='/register'>Register</Link></li>
+				</ul>
+			</div>
 		)
 	}
 }
